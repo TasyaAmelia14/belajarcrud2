809198628dd2ec2103037338c539de445ea1e80e
Tasya Amelia <tsyamelia1466@gmail.com>
2023-08-09 08:41:14 +0700

    first commit
    Install Laravel, Routte, Slicing Template, Dynamic Menu/Content

---
+++ resources/views/kelas.blade.php
@@ -0,0 +1,8 @@
+<h1>Kelas Views</h1>
+<br>
+<a href="{{ url('home') }}">Home</a>
+<a href="about">about</a>
+<a href="contact">contact</a>
+<a href="guru">Guru</a>
+<a href="siswa">Siswa</a>
+<a href="kelas">Kelas</a>
